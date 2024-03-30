3d8c19ca6ee1e560815b80781b997fd4711d3c52
RenatZubakin <zubakinr03@mail.ru>
2024-03-30 13:30:31 +0500

    t

--- src/Models/Game.cs
+++ src/Models/Game.cs
@@ -8,13 +8,58 @@ public class Game
 
     public Game()
     {
-        Cells = new int[3, 3];
+        Cells = new int[4, 4];
     }
     
     public void Move(Direction direction)
     {
-        
+        switch (direction)
+        {
+            case Direction.Up:
+                MoveUp();
+                break;
+            case Direction.Right:
+                MoveRight();
+                break;
+            case Direction.Down:
+                MoveDown();
+                break;
+            case Direction.Left:
+                MoveLeft();
+                break;
+            
+        }
     }
+
+    private bool IsEmptyCell(int x,int y)
+    {
+        return Cells[y, x]==0;
+    }
+
+    private void MoveUp()
+    {
+        for (var y = 1; y < Cells.Length; y++)
+        {
+
+        }
+    }
+    
+    private void MoveRight()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    private void MoveDown()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    private void MoveLeft()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
 
     public GameDto ToDTO()
     {
